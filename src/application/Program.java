b4837737f66d2b1714ad0b543aef1b03a048f985
AqlCaraMsm <gabrielsw3222@gmail.com>
2020-04-10 15:45:25 -0300

    Fist class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args){
-        System.out.println("aquele filme q eu tenho q ver");
+        Position p = new Position(3,5);
+        System.out.println ( p );
     }
 }
